348bc84886a6665ed40d3a516b336f2d259a8c63
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2021-03-25 00:15:28 -0400

    Fixed Sigin Bug

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb+srv://surbhi:surbhi@clustercontactapp.jzyeq.mongodb.net/contactApp?retryWrites=true&w=majority';
+                      'mongodb://surbhi:surbhi@clustercontactapp-shard-00-00.jzyeq.mongodb.net:27017,clustercontactapp-shard-00-01.jzyeq.mongodb.net:27017,clustercontactapp-shard-00-02.jzyeq.mongodb.net:27017/contactApp?ssl=true&replicaSet=atlas-vg3i2n-shard-0&authSource=admin&retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = (
                           	process.env.TEST_DATABASE_URL ||
-	                   'mongodb+srv://surbhi:surbhi@clustercontactapp.jzyeq.mongodb.net/contactApp?retryWrites=true&w=majority');
+	                   'mongodb://surbhi:surbhi@clustercontactapp-shard-00-00.jzyeq.mongodb.net:27017,clustercontactapp-shard-00-01.jzyeq.mongodb.net:27017,clustercontactapp-shard-00-02.jzyeq.mongodb.net:27017/contactApp?ssl=true&replicaSet=atlas-vg3i2n-shard-0&authSource=admin&retryWrites=true&w=majority');
 exports.PORT = process.env.PORT || 8080;
 
